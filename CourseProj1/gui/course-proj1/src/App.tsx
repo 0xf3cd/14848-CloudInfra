d1f4a7520bb1bd0e77ac39a62055de0f94bb508d
0xf3cd <nq.maigre@gmail.com>
2021-10-29 03:21:21 -0400

    Implemented the basic homepage

--- CourseProj1/gui/course-proj1/src/App.tsx
+++ CourseProj1/gui/course-proj1/src/App.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Alert, Container, Row, Col, Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const processClick = (choice: number): void => {
+  console.log(`Received choice: ${choice}`);
+};
+
+interface MyCardProps {
+  title: string;
+  clickChoice: number;
+};
+
+const MyCard = ({title, clickChoice}: MyCardProps) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          Click "Run".
+        </Card.Text>
+        <Button variant="success" onClick={() => { processClick(clickChoice); }}>Run</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 const App = () => {
   return (
     <div className="App">
-      <p>Course Project 1</p>
+      {/* The header of the page */}
+      <Alert variant="success">
+        <Alert.Heading>Big Data Processing Application</Alert.Heading>
+        <p>Course Project 1</p>
+      </Alert>
+      <Container>
+        <Row>
+          <Col><MyCard title="Apache Hadoop" clickChoice={1} /></Col>
+          <Col><MyCard title="Apache Spark" clickChoice={2} /></Col>
+        </Row>
+        <Row>
+          <Col><MyCard title="Jupyter Notebook" clickChoice={3} /></Col>
+          <Col><MyCard title="SonarQube & SonarScanner" clickChoice={4} /></Col>
+        </Row>
+      </Container>
     </div>
   );
 };
