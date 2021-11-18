bbc985c73459a495f5badaa2d25fb5fe8e28ae7d
0xf3cd <nq.maigre@gmail.com>
2021-11-18 17:42:54 -0500

    Implemented the mapper and reducer

---
+++ HW4/src/mapper.py
@@ -0,0 +1,24 @@
+import sys
+
+def parse_line(line):
+  day = int(line[15:24])
+  temp = int(line[87:93])
+  quality = int(line[93])
+  return (day, temp, quality)
+
+def validate(temp, quality):
+  if abs(temp) == 9999:
+    return False
+  if quality not in [0, 1, 4, 5, 9]:
+    return False
+  return True
+
+for line in sys.stdin:
+  line = line.strip()
+  day, temp, quality = parse_line(line)
+
+  # Skip the invalid line(s).
+  if not validate(temp, quality):
+    continue
+
+  print(f'{day} {temp}')
---
+++ HW4/src/reducer.py
@@ -0,0 +1,27 @@
+import sys
+
+cur_day = None
+cur_max_temp = -9999
+
+for line in sys.stdin:
+  # Parse and validate the intpu line.
+  line = line.strip()
+  day, temp = line.split(' ')
+  try:
+    day = int(day)
+    temp = int(temp)
+  except ValueError:
+    continue
+
+  # Check if we need to update the metadata.
+  if day != cur_day:
+    if cur_day:
+      print(f'{cur_day}\t{cur_max_temp}')
+    cur_day = day
+    cur_max_temp = -9999
+
+  if cur_max_temp < temp:
+    cur_max_temp = temp
+
+if cur_day:
+  print(f'{cur_day}\t{cur_max_temp}')
